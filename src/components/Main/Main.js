0f817eb7ea74ddb07fe1e042f585d8d4880e6ac9
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-03-19 21:17:36 +0700

    add main component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import AddMovieForm from "../AddMovieForm/AddMovieForm";
+import Hero from "../Hero/Hero";
+import Movies from "../Movies/Movies";
+
+function Main() {
+  return (
+    <main>
+      <Hero />
+      <Movies />
+      <AddMovieForm />
+    </main>
+  );
+}
+
+export default Main;
