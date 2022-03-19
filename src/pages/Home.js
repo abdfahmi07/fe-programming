9cfa5c009ec9372b2f5fb69cf4ecbbea966e7721
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-03-19 21:15:20 +0700

    add home page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import Footer from "../components/Footer/Footer";
+import Main from "../components/Main/Main";
+import Navbar from "../components/Navbar/Navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
