c32f4a87c7a2ad35d9314191309ce12d8a6b3362
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-03-19 21:18:03 +0700

    add hero component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,29 @@
+import styles from "./Hero.module.css";
+
+function Hero() {
+  return (
+    <div className={styles.container}>
+      <section className={styles.hero}>
+        <div className={styles.hero__left}>
+          <h2 className={styles.hero__title}>Spiderman</h2>
+          <h3 className={styles.hero__genre}>Genre: Drama, Thriller</h3>
+          <p className={styles.hero__description}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            suscipit sapiente ipsum soluta at repudiandae deserunt incidunt
+            error illum reprehenderit!
+          </p>
+          <button className={styles.hero__button}>Watch</button>
+        </div>
+        <div className={styles.hero__right}>
+          <img
+            className={styles.hero__image}
+            src="https://picsum.photos/600/400"
+            alt="hero"
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Hero;
