f31965c9a8872e84fbb9d5e170acb52f6ac6e01b
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-04-06 23:30:59 +0700

    handle form using state

--- src/components/AddMovieForm/AddMovieForm.js
+++ src/components/AddMovieForm/AddMovieForm.js
@@ -1,6 +1,47 @@
+import { nanoid } from "nanoid";
+import { useState } from "react";
+import Alert from "../Alert/Alert";
 import styles from "./AddMovieForm.module.css";
 
-function AddMovieForm() {
+function AddMovieForm(props) {
+  const { movies, setMovies } = props;
+
+  const [title, setTitle] = useState("");
+  const [year, setYear] = useState("");
+  const [isTitleError, setIsTitleError] = useState(false);
+  const [isYearError, setIsYearError] = useState(false);
+
+  function handleTitle(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleYear(e) {
+    setYear(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (title === "") {
+      setIsTitleError(true);
+    } else if (year === "") {
+      setIsTitleError(false);
+      setIsYearError(true);
+    } else {
+      const movie = {
+        id: nanoid(),
+        title,
+        year,
+        type: "Movie",
+        poster: "https://picsum.photos/300/400",
+      };
+
+      setMovies([...movies, movie]);
+      setTitle("");
+      setYear("");
+      setIsTitleError(false);
+      setIsYearError(false);
+    }
+  }
   return (
     <div className={styles.container}>
       <section className={styles.movie__form}>
@@ -13,18 +54,32 @@ function AddMovieForm() {
         </div>
         <div className={styles.movie__form__right}>
           <h2 className={styles.movie__form__title}>Add Movie</h2>
-          <form className={styles.form} action="#">
+          <form className={styles.form} onSubmit={handleSubmit}>
             <div className={styles.form__group}>
               <label className={styles.form__label} htmlFor="title">
                 Title
               </label>
-              <input id="title" className={styles.form__input} type="text" />
+              <input
+                id="title"
+                className={styles.form__input}
+                type="text"
+                value={title}
+                onChange={handleTitle}
+              />
+              {isTitleError && <Alert>Title Wajib Diisi</Alert>}
             </div>
             <div className={styles.form__group}>
               <label className={styles.form__label} htmlFor="year">
                 Year
               </label>
-              <input id="year" className={styles.form__input} type="text" />
+              <input
+                id="year"
+                className={styles.form__input}
+                type="text"
+                value={year}
+                onChange={handleYear}
+              />
+              {isYearError && <Alert>Year Wajib Diisi</Alert>}
             </div>
             <div className={styles.form__group}>
               <button className={styles.form__button}>Submit</button>
