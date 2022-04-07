de3df6644e785608bbda23ab406d66cb7685b3d0
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-04-07 16:52:04 +0700

    display type movie description

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,13 +1,19 @@
 import styles from "./Movie.module.css";
 
 function Movie(props) {
-  // Desctructing props movie
-  const { title, year, poster } = props.movie;
+  // Desctructing props
+  const { title, year, type, poster } = props.movie;
+
+  // Formatting text for capitalize first letter
+  function capitalizeFirstLetter(letter) {
+    return letter.charAt(0).toUpperCase() + letter.slice(1);
+  }
 
   return (
     <div className={styles.movie}>
       <img className={styles.movie__image} src={poster} alt="movie" />
       <h3 className={styles.movie__title}>{title}</h3>
+      <p className={styles.movie__type}>{capitalizeFirstLetter(type)}</p>
       <p className={styles.movie__date}>{year}</p>
     </div>
   );
