ac927535d17abcc51c8aeb5532306a4817c3e2dd
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-05-28 16:54:04 +0700

    add property on theme

---
+++ src/utils/constant/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+  colors: {
+    primary: "#4895ef",
+    secondary: "#b5179e",
+  },
+
+  sizes: {
+    sm: {
+      fontSize: "0.8rem",
+      padding: "0.2rem 0.5rem",
+    },
+    md: {
+      fontSize: "1rem",
+      padding: "0.5rem 1rem",
+    },
+    lg: {
+      fontSize: "1.3rem",
+      padding: "0.5rem 1rem",
+    },
+  },
+};
+
+export default theme;
