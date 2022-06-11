787968265b946d0a136936dee7c7aa7ca0248bd4
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-06-11 15:32:06 +0700

    add some icon to library and import them

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+import "./library/fontawesome";
+
 ReactDOM.render(
   <React.StrictMode>
     {/**
---
+++ src/library/fontawesome.js
@@ -0,0 +1,8 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faPlay,
+  faInfo,
+  faCircleInfo,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faPlay, faInfo, faCircleInfo);
