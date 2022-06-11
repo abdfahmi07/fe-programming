8ca321a84f3d8bd123f0e915aefce4bcc9634f74
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-06-11 20:01:57 +0700

    attach provider redux and send props store

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+// Import Porvider: untuk menghubungkan react dan redux
+import { Provider } from "react-redux";
 import "./library/fontawesome";
+import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/**
-     * Bungkus/Hubungkan aplikasi dengan Browser Router
-     * Agar URL browser dapat dimanipulasikan oleh React Router
-     */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* Bungkus App dengan Provider: agar dapat mengakses store */}
+    <Provider store={store}>
+      {/**
+       * Bungkus/Hubungkan aplikasi dengan Browser Router
+       * Agar URL browser dapat dimanipulasikan oleh React Router
+       */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
