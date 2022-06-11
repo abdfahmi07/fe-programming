2ce42b544f3484c28ccae8afeeaf68e5c963f393
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-06-11 20:02:38 +0700

    add movie slice

---
+++ src/features/moviesSlice/index.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import data from "../../utils/constant/data";
+
+/**
+ * Buat slice : generate action dan reducer
+ * menerima params object: name, initialState, reducers
+ */
+
+const moviesSlice = createSlice({
+  name: "Movies Slice",
+  initialState: {
+    movies: data,
+  },
+  reducers: {
+    addMovie(state, action) {
+      state.movies.push(action.payload);
+    },
+    deleteMovie() {},
+    updateMovie(state, action) {
+      state.movies = action.payload;
+    },
+  },
+});
+
+/**
+ * generate action dan reducer
+ * nama action diambil dari nama reducer
+ * action untuk menjalankan reducer
+ * reducer untuk meng-update state
+ */
+
+const moviesReducer = moviesSlice.reducer;
+const { addMovie, deleteMovie, updateMovie } = moviesSlice.actions;
+
+export default moviesReducer;
+export { addMovie, deleteMovie, updateMovie };
