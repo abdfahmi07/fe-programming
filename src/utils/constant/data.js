b5694e2c3d4a61669eb7bb2fd2f009ea7d2faf28
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-06-20 20:02:07 +0700

    change imdbid to id

--- src/utils/constant/data.js
+++ src/utils/constant/data.js
@@ -1,6 +1,6 @@
 const data = [
   {
-    id: "tt10872600",
+    id: "634649",
     title: "Spider-Man",
     year: "2021",
     type: "movie",
@@ -8,7 +8,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BZWMyYzFjYTYtNTRjYi00OGExLWE2YzgtOGRmYjAxZTU3NzBiXkEyXkFqcGdeQXVyMzQ0MzA0NTM@._V1_SX300.jpg",
   },
   {
-    id: "tt2953050",
+    id: "568124",
     title: "Encanto",
     year: "2021",
     type: "movie",
@@ -16,7 +16,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BNjE5NzA4ZDctOTJkZi00NzM0LTkwOTYtMDI4MmNkMzIxODhkXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_SX300.jpg",
   },
   {
-    id: "tt7991608",
+    id: "512195",
     title: "Red Notice",
     year: "2021",
     type: "movie",
@@ -24,7 +24,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BZmRjODgyMzEtMzIxYS00OWY2LTk4YjUtMGMzZjMzMTZiN2Q0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
   },
   {
-    id: "tt12689248",
+    id: "753232",
     title: "The Commando",
     year: "2022",
     type: "movie",
@@ -32,7 +32,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BYTQ5ZjM3YmQtNTQ4ZC00NzBmLTk5YTUtN2I1NWRhYzcyNzgzXkEyXkFqcGdeQXVyNDA1NDA2NTk@._V1_SX300.jpg",
   },
   {
-    id: "tt14060094",
+    id: "823625",
     title: "Blacklight",
     year: "2022",
     type: "movie",
@@ -40,7 +40,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BY2ViYmNhNzEtZDJhNS00YzM4LTkzZWItZDJiNzc5ZDExOWZmXkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_SX300.jpg",
   },
   {
-    id: "tt8097030",
+    id: "508947",
     title: "Turning Red",
     year: "2022",
     type: "movie",
@@ -48,7 +48,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BNjY0MGEzZmQtZWMxNi00MWVhLWI4NWEtYjQ0MDkyYTJhMDU0XkEyXkFqcGdeQXVyODc0OTEyNDU@._V1_SX300.jpg",
   },
   {
-    id: "tt14128670",
+    id: "800510",
     title: "Kimi",
     year: "2022",
     type: "movie",
@@ -56,7 +56,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BYjFmOWUwYjgtM2UyYS00M2FmLTgwNmUtMWIwNTc2ZTgzNmRhXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_SX300.jpg",
   },
   {
-    id: "tt13634480",
+    id: "774825",
     title: "The Ice Age",
     year: "2022",
     type: "movie",
@@ -64,7 +64,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BNDJmMzQyMzAtMzMxMy00NTI3LTgzOGMtZDU3Yzc4MjRjNzkwXkEyXkFqcGdeQXVyMTA1OTcyNDQ4._V1_SX300.jpg",
   },
   {
-    id: "tt9848626",
+    id: "585083",
     title: "Transylvania 4",
     year: "2022",
     type: "movie",
@@ -72,7 +72,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BZWM4MzUyYTAtMGFiYy00MmRkLWIyNDktMWFlMDlmZjJmNWZmXkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_SX300.jpg",
   },
   {
-    id: "tt9032400",
+    id: "524434",
     title: "Eternals",
     year: "2021",
     type: "movie",
@@ -80,7 +80,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BMTExZmVjY2ItYTAzYi00MDdlLWFlOWItNTJhMDRjMzQ5ZGY0XkEyXkFqcGdeQXVyODIyOTEyMzY@._V1_SX300.jpg",
   },
   {
-    id: "tt7740496",
+    id: "597208",
     title: "Nightmare Alley",
     year: "2021",
     type: "movie",
@@ -88,7 +88,7 @@ const data = [
       "https://m.media-amazon.com/images/M/MV5BOTI4NDhhNGEtZjQxZC00ZTRmLThmZTctOGJmY2ZlOTc0ZGY0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
   },
   {
-    id: "tt11755740",
+    id: "632727",
     title: "Texas Chainsaw",
     year: "2022",
     type: "movie",
