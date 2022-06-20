33e9a0bd1b7d0919064477203897f897ea5578be
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-06-20 20:02:39 +0700

    remove query params region

--- src/utils/constant/endpoints.js
+++ src/utils/constant/endpoints.js
@@ -2,9 +2,9 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const ENDPOINTS = {
-  POPULAR: `${BASE_URL}/movie/popular?api_key=${API_KEY}&region=ID`,
-  NOW_PLAYING: `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&region=ID`,
-  TOP_RATED: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&region=ID`,
+  POPULAR: `${BASE_URL}/movie/popular?api_key=${API_KEY}`,
+  NOW_PLAYING: `${BASE_URL}/movie/now_playing?api_key=${API_KEY}`,
+  TOP_RATED: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`,
   HERO: `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`,
   RECOMMENDATION: (id) => {
     return `${BASE_URL}/movie/${id}/recommendations?api_key=${API_KEY}`;
