24aa255683d1a02f79650e8250b94810c50642fd
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-07-02 00:35:17 +0700

    add toMonthName helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,4 +3,13 @@ const isMobileRgx =
     navigator.userAgent
   );
 
-export { isMobileRgx };
+const toMonthName = (monthNum) => {
+  const date = new Date();
+  date.setMonth(monthNum - 1);
+
+  return date.toLocaleString("id-ID", {
+    month: "long",
+  });
+};
+
+export { isMobileRgx, toMonthName };
