c7a860f8adb610a3db8025469bec86894d9e317a
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-07-04 21:23:01 +0700

    add page upcoming movies

---
+++ src/pages/movie/Upcoming.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Hero from "../../components/Hero";
+import Movies from "../../components/Movies";
+import {
+  openMenu,
+  openSearch,
+  setIsLoading,
+} from "../../features/featuresSlice";
+import { updateMovies } from "../../features/moviesSlice";
+import ENDPOINTS from "../../utils/constant/endpoints";
+
+function UpcomingMovie() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(openSearch(false));
+    dispatch(openMenu(false));
+    getUpcomingMovies();
+  }, []);
+
+  async function getUpcomingMovies() {
+    dispatch(setIsLoading(true));
+    const response = await axios(ENDPOINTS.MOVIE.UPCOMING);
+    dispatch(updateMovies(response.data.results));
+    dispatch(setIsLoading(false));
+  }
+
+  return (
+    <div>
+      <Hero endpoint={ENDPOINTS.MOVIE.UPCOMING} type="MOVIE" />
+      <Movies title="Upcoming Movies" />
+    </div>
+  );
+}
+
+export default UpcomingMovie;
