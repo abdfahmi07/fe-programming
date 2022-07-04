988f9c09f8299e0b547623be93113375fa825273
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-07-04 21:23:44 +0700

    add tv show's routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import GlobalStyle from "./components/GlobalStyle";
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import CreateMovie from "./pages/movie/Create";
-import Detail from "./pages/movie/Detail";
+import DetailMovies from "./pages/movie/Detail";
 import NowPlayingMovie from "./pages/movie/NowPlaying";
 import PopularMovie from "./pages/movie/Popular";
 import SearchMovie from "./pages/movie/SearchMovie";
 import TopRatedMovie from "./pages/movie/TopRated";
+import UpcomingMovie from "./pages/movie/Upcoming";
+import AiringTodayTVShow from "./pages/tvshow/AiringToday";
+import DetailTVShows from "./pages/tvshow/Detail";
+import OnTheAirTVShow from "./pages/tvshow/OnTheAir";
+import PopularTVShow from "./pages/tvshow/Popular";
+import TopRatedTVShow from "./pages/tvshow/TopRated";
 import theme from "./utils/constant/theme";
 
 function App() {
@@ -26,9 +32,15 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/movie/create" element={<CreateMovie />} />
             <Route path="/movie/popular" element={<PopularMovie />} />
-            <Route path="/movie/now" element={<NowPlayingMovie />} />
-            <Route path="/movie/top" element={<TopRatedMovie />} />
-            <Route path="/movie/:id" element={<Detail />} />
+            <Route path="/movie/now-playing" element={<NowPlayingMovie />} />
+            <Route path="/movie/top-rated" element={<TopRatedMovie />} />
+            <Route path="/movie/upcoming" element={<UpcomingMovie />} />
+            <Route path="/movie/:id" element={<DetailMovies />} />
+            <Route path="/tv/popular" element={<PopularTVShow />} />
+            <Route path="/tv/airing-today" element={<AiringTodayTVShow />} />
+            <Route path="/tv/on-the-air" element={<OnTheAirTVShow />} />
+            <Route path="/tv/top-rated" element={<TopRatedTVShow />} />
+            <Route path="/tv/:id" element={<DetailTVShows />} />
             <Route path="/movie/search/:query" element={<SearchMovie />} />
           </Routes>
         </Layout>
